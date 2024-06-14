10dd30fd7a942ac31dfe879db288fd47fed8bcb8
NeelPatel122 <pneel122@gmail.com>
2024-06-13 22:07:27 -0400

    Login and registration Api created

--- LoginApp/Controllers/UsersController.cs
+++ LoginApp/Controllers/UsersController.cs
@@ -22,15 +22,58 @@ namespace LoginApp.Controllers
             {
                 return BadRequest(ModelState);
             }
-            dbContext.Users.Add(new User
+            var objUser = dbContext.Users.FirstOrDefault(x => x.Email == userDTO.Email);
+            if (objUser == null)
             {
-                FirstName = userDTO.FirstName,
-                LastName = userDTO.LastName,
-                Email = userDTO.Email,
-                Password = userDTO.Password
-            });
-            dbContext.SaveChanges();
-            return Ok("User registered successfully!");
+                dbContext.Users.Add(new User
+                {
+                    FirstName = userDTO.FirstName,
+                    LastName = userDTO.LastName,
+                    Email = userDTO.Email,
+                    Password = userDTO.Password
+                });
+                dbContext.SaveChanges();
+                return Ok("User registered successfully!");
+            }
+            else
+            {
+                return BadRequest("User already exists with the same email address");
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginDTO loginDTO) 
+        { 
+            var user = dbContext.Users.FirstOrDefault(x=>x.Email == loginDTO.Email &&
+            x.Password == loginDTO.Password);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers() 
+        {
+            return Ok(dbContext.Users.ToList());
+        }
+
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser(int id) 
+        {
+            var user = dbContext.Users.FirstOrDefault(x=>x.UserId == id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NoContent();
+            }
         }
     }
 }
