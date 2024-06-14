559fd80af92f756f7823f447dfdd749f895e9715
NeelPatel122 <pneel122@gmail.com>
2024-06-13 21:47:47 -0400

    Add project files.

---
+++ LoginApp/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using LoginApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+        public UsersController(AppDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        [HttpPost]
+        [Route("Registration")]
+        public IActionResult Registration(UserDTO userDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            dbContext.Users.Add(new User
+            {
+                FirstName = userDTO.FirstName,
+                LastName = userDTO.LastName,
+                Email = userDTO.Email,
+                Password = userDTO.Password
+            });
+            dbContext.SaveChanges();
+            return Ok("User registered successfully!");
+        }
+    }
+}
---
+++ LoginApp/Models/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginApp.Models
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
